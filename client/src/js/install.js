276d1a9cf91b1e17fd0a40247249f8035ff9d8df
Gestan <104741659+Gestan24@users.noreply.github.com>
2022-10-02 18:37:21 -0500

    added logic for install button

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,27 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+window.addEventListener('beforeinstallprompt', (event) => {
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    event.preventDefault();
+    butInstall.style.visibility = 'visible';
+
+});
+
+
+butInstall.addEventListener('click', async () => {
+
+    event.prompt();
+
+    butInstall.setAttribute('disabled', true);
+
+    butInstall.textContent = 'Installed';
+
+});
+
+
+window.addEventListener('appinstalled', (event) => {
+
+    console.log('👍', 'appinstalled', event);
+});
